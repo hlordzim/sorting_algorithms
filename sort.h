60be1b340c4d4a533e07246eed4dd4a3cf78cf56
hlordzim <hlordzim.mh@gmail.com>
2024-02-24 17:51:42 -0800

    11. Quick Sort - Hoare Partition scheme

--- sort.h
+++ sort.h
@@ -45,7 +45,7 @@ void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
 		char flow);
 void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
 void bitonic_sort(int *array, size_t size);
-
+void quick_sort_hoare(int *array, size_t size);
 
 
 
